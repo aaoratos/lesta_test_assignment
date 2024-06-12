72b1d9f7b7cbf71084bb0ed6305e2617a4ac5fd5
Anton Anufriev <aaoratos@pm.me>
2024-06-12 21:18:44 +0300

    main.cpp: minor formatting refactor

--- is_even/cpp/main.cpp
+++ is_even/cpp/main.cpp
@@ -15,17 +15,19 @@ concept Stringlike = std::is_convertible_v<T, std::string_view>;
 
 } // namespace constraints
 
-template <constraints::Integral T> constexpr bool is_even(T val)
+template <constraints::Integral T>
+constexpr auto is_even(T val) -> bool
 {
   return ((val >> 1) << 1) == val;
 }
 
 template <constraints::Integral T>
-constexpr T strtoint(const constraints::Stringlike auto &str)
+constexpr T strtoint(const constraints::Stringlike auto& str)
 {
   try {
     return static_cast<T>(std::stoll(str));
-  } catch (const std::exception &) {
+  }
+  catch (const std::exception &) {
     throw std::invalid_argument("Invalid number: " + str);
   }
 }
@@ -45,7 +47,7 @@ int main([[maybe_unused]] const int argc,
               << (is_even(n) ? "Even" : "Odd")
               << std::endl;
   }
-  catch (const std::exception &e) {
+  catch (const std::exception& e) {
     std::cerr << "[Error]: " << e.what() << std::endl;
     return EXIT_FAILURE;
   }
